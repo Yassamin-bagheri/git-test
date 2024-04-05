ec31e90f5f79ac18008c75817c602c15dcd3de61
unknown <bagheryyasamin83@gmail.com>
2024-04-05 10:48:59 +0330

    finalize change

--- script.js
+++ script.js
@@ -20,16 +20,15 @@ const students = [
         scores: [12, 8, 19],
     },
 ];
-let totalSum = 0;
-for (let student of students) {
-    let sum = 0;
-    for (let score of student.scores) {
-        sum += score;
-    }
-    console.log(student.fName, student.lName, ":", sum / student.scores.length);
-    totalSum += sum / student.scores.length;
-}
-console.log(totalSum / students.length);
+// let totalSum = 0;
+// for (let student of students) {
+//     let sum = 0;
+//     for (let score of student.scores) {
+//         sum += score;
+//     }
+//     console.log(student.fName, student.lName, ":", sum / student.scores.length);
+//     totalSum += sum / student.scores.length;
+// }
+// console.log(totalSum / students.length);
 
 
-// mehrad kahbazi
